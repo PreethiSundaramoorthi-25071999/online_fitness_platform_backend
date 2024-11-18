1184eaf508ec904306bf0fed83c3792f083a48d9
PreethiSundaramoorthi-25071999 <preethisundhar.ps@gmail.com>
2024-11-18 23:47:31 +0530

    new

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const trainerRoutes = require('./routes/trainer-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/trainer', trainerRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect('mongodb+srv://preethisundharps:WLvifNNAPEe22u78@cluster0.102of.mongodb.net/fitness_platform')
+  .then(() => {
+    console.log('connection success')
+    app.listen(5001);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
---
+++ controllers/users-controllers.js
@@ -0,0 +1,73 @@
+const { validationResult } = require('express-validator');
+
+const HttpError = require('../models/http-error');
+const User = require('../models/user');
+const Trainer=require('../models/trainer');
+const Member=require('../models/member');
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, '-password');
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching users failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+  res.json({users: users.map(user => user.toObject({ getters: true }))});
+};
+//
+const signup = async (req, res, next) => {
+    const { name, email, password,type } = req.body;
+  const UserType=type==="trainer"?Trainer:Member
+  const createdUser = new UserType({
+    name,
+    email,
+    profilePic: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
+    password,
+    type
+  });
+  try {
+    await createdUser.save();
+  } catch (err) {
+    console.log(err)
+    const error = new HttpError(
+      'Signing up failed, please try again.',
+      500
+    );
+    return next(error);
+  }
+
+  res.status(201).json({user: createdUser.toObject({ getters: true })});
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      'Invalid credentials, could not log you in.',
+      401
+    );
+    return next(error);
+  }
+
+  res.json({message: 'Logged in!',type:existingUser.type});
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
